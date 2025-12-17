803d81b0044eb7266e768329452778c657fd111b
shiva singh <shivanand1801@gmail.com>
2025-12-17 11:04:37 +0530

    correct th value

--- Module/Virtual_network/main.tf
+++ Module/Virtual_network/main.tf
@@ -1,15 +1,15 @@
 resource "azurerm_virtual_network" "vnet" {
   for_each            = var.vnets
-  name                = each.valu.name
-  location            = each.valu.location
-  resource_group_name = each.valu.resource_group_name
-  address_space       = each.valu.address_space
+  name                = each.value.name
+  location            = each.value.location
+  resource_group_name = each.value.resource_group_name
+  address_space       = each.value.address_space
 
 
   dynamic "subnet" {
-    for_each = each.valu.subnets
+    for_each = each.value.subnets
     content {
-      name             = subnet.valu.name
+      name             = subnet.value.name
       address_prefixes = subnet.value.address_prefixes
     }
   }
