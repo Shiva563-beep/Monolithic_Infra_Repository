1047760110eedfa924ff85c8941866da4adabc6f
shiva singh <shivanand1801@gmail.com>
2025-12-17 10:57:00 +0530

    module correction

--- Environment/Preprod/variable.tf
+++ Environment/Preprod/variable.tf
@@ -31,7 +31,12 @@ variable "vnets" {
       location            = string
       resource_group_name = string
       address_space       = list(string)
-      subnets             = map(object)
+      subnets             = map(object({
+         name             = string
+        address_prefixes = list(string)
+
+        }
+      ))
     }
   ))
 }
