cd9a8d6843b1a41852b0578c4162fd997cbb103d
shiva singh <shivanand1801@gmail.com>
2025-12-20 17:06:03 +0530

    module correction done

--- Environment/Preprod/main.tf
+++ Environment/Preprod/main.tf
@@ -1,14 +1,18 @@
-module "rg_name" {
-  source  = "../../Module/resource_group"
-  rg_name = var.rg_name
+  module "rg_name" {
+    source  = "../../Module/resource_group"
+    rg_name = var.rg_name
 
-}
+  }
 
-module "stgs_name" {
-  depends_on = [module.rg_name]
+  module "stgs_name" {
+    depends_on = [module.rg_name]
 
-  source = "../../Module/Storage_account"
-  stgs   = var.stgs
+    source = "../../Module/Storage_account"
+    stgs   = var.stgs
 
-}
+  }
 
+  module "vnet" {
+    source = "../../Module/Virtual_network"
+    vnets = var.vnets
+  }
