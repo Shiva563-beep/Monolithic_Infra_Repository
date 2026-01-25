6d777288e6aa89a035b1b7d7c3cdca39bbaea80b
shiva singh <shivanand1801@gmail.com>
2026-01-25 23:07:11 +0530

    correct storage account added

--- Environment/Preprod/bacend.tf
+++ Environment/Preprod/bacend.tf
@@ -1,8 +1,8 @@
 terraform {
   backend "azurerm" {
     resource_group_name  = "shiva-rg"
-    storage_account_name = "shivastg47118"
+    storage_account_name = "shivastg7118"
     container_name       = "file"
-    key                  = "terraform.tfstate"
+    key                  = "terraformtfstate"
   }
 }
